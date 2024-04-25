e57ead101480584ffef0767d9cf59b2ab34fd8b3
znair96 <zubnair2396@gmail.com>
2024-04-26 01:08:15 +0530

    initial commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-useless-catch */
+import conf from "../config/config";
+
+import { Client,Account,ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;
+    constructor(){
+        this.client.setEndpoint(conf.appWriteUrl).setProject(conf.appWriteProjectId);
+        this.account = new Account(this.client);
+    }
+    async createAccount({name,email,password}){
+        try {
+            const userAccount = await this.account.create(ID.unique,email,password,name);
+            if(userAccount){
+                return this.login({email,password});
+            }
+            return userAccount;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async login({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password);
+        } catch (error) {
+            throw error;
+        }
+    }
+    async getCurrentUser(){
+        try {
+            await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+export default authService;
