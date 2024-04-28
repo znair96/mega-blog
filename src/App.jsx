410fab96478734bf671d3110e4504ddb92c84c47
znair96 <zubnair2396@gmail.com>
2024-04-28 22:56:38 +0530

    creation of store,auth slice and resuable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Footer, Header } from './components';
 function App() {
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser().then((userData)=>{
+      if(userData){
+        dispatch(login({userData}));
+      } else{
+        dispatch(logout());
+      }
+    }).catch(()=>{
+      dispatch(logout());
+    })
+    .finally(()=>{
+        setLoading(false);
+    })
+  },[]);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+       <div className='w-full block'>
+         <Header/>
+           <main>
+              {/*<Outlet/>*/}
+           </main>
+         <Footer/>
+       </div>
+    </div>
+  ) : null
 }
 
 export default App
