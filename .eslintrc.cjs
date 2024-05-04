5e7c1c749254660958be3738d7a079a3d8174fc8
znair96 <zubnair2396@gmail.com>
2024-05-04 15:10:55 +0530

    added login and sign up forms along with appwrite service auth

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
+    // 'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
@@ -12,10 +11,10 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    // 'react/jsx-no-target-blank': 'off',
+    // 'react-refresh/only-export-components': [
+    //   'warn',
+    //   { allowConstantExport: true },
+    // ],
   },
 }
